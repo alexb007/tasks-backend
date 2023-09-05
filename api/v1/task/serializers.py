8fbdee437409cdc28e64d816979690f94c94a9b4
Bakhrom <alex960126@gmail.com>
2023-09-05 16:14:49 +0500

    task api complete, assign, comment comment api viewset added

--- api/v1/task/serializers.py
+++ api/v1/task/serializers.py
@@ -1,9 +1,35 @@
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserSerializer
 from rest_framework import serializers
 
-from apps.task.models import Task
+from apps.task.models import Task, TaskComment
+
+User = get_user_model()
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    creator = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
+    def to_representation(self, instance):
+        data = super(TaskSerializer, self).to_representation(instance)
+        data['creator'] = UserSerializer(instance.creator).data
+        if instance.assigned is not None:
+            data['assigned'] = UserSerializer(instance.assigned).data
+        return data
+
     class Meta:
         model = Task
+        fields = '__all__'
+
+
+class TaskAssignSerializer(serializers.Serializer):
+    user = serializers.PrimaryKeyRelatedField(write_only=True, queryset=User.objects.all())
+
+
+class TaskCommentSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+    task = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = TaskComment
         fields = '__all__'
--- api/v1/task/views.py
+++ api/v1/task/views.py
@@ -1,11 +1,100 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from django.http import QueryDict
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
-from api.v1.task.serializers import TaskSerializer
-from apps.task.models import Task
+from api.v1.task.serializers import TaskSerializer, TaskAssignSerializer, TaskCommentSerializer
+from apps.task.models import Task, TaskComment
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Task.objects.all()
+    filterset_fields = ('creator', 'assigned', 'state')
+
+    def get_serializer_class(self):
+        if action == 'assign':
+            return TaskAssignSerializer
+        return self.serializer_class
+
+    @swagger_auto_schema(
+        operation_description='POST /tasks/{id}/complete/',
+        request_body=openapi.Schema(type=openapi.TYPE_OBJECT, properties=None), responses={
+            status.HTTP_200_OK: openapi.Response(
+                type=openapi.TYPE_OBJECT,
+                description='Success Response',
+                examples={
+                    "application/json": {
+                        "success": True,
+                        "message": "task <TaskName> with id <task_id> marked as done"
+                    }
+                }
+            )
+        })
+    @action(detail=True, methods=['patch'])
+    def complete(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.mark_done()
+        task.save()
+        return Response({
+            'success': True,
+            'message': f'task {task.name} with id {task.id} marked as done'
+        })
+
+    @swagger_auto_schema(
+        operation_description='POST /tasks/{id}/assign/',
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                type=openapi.TYPE_OBJECT,
+                schema=TaskSerializer,
+                description='Success Response',
+            ),
+        })
+    @action(detail=True, methods=['patch'], serializer_class=TaskAssignSerializer)
+    def assign(self, request, *args, **kwargs):
+        task = self.get_object()
+        serializer = TaskAssignSerializer(data=request.data)
+        if serializer.is_valid():
+            task.assigned = serializer.validated_data['user']
+            task.save()
+            return Response(TaskSerializer(task).data)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_description='POST /tasks/{id}/comment/',
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                type=openapi.TYPE_OBJECT,
+                schema=TaskCommentSerializer,
+                description='Success Response',
+            ),
+        })
+    @action(detail=True, methods=['post'], serializer_class=TaskCommentSerializer)
+    def comment(self, request, *args, **kwargs):
+        task = self.get_object()
+        try:
+            task_comment = TaskComment.objects.create(
+                task=task,
+                comment=request.data.get('comment'),
+                user=request.user,
+            )
+            return Response(TaskCommentSerializer(task_comment).data)
+        except Exception as ex:
+            print(ex)
+            return Response(
+                ex,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class TaskCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskCommentSerializer
+    queryset = TaskComment.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    filterset_fields = ('task', 'user',)
